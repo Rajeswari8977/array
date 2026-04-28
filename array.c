51fdb5bac5d346d5b1b5af78bf4526bba67c1362
Rajeswari8977 <allampallirajeswari9@gmail.com>
2026-04-28 15:37:18 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+    int size;
+    printf("Enter the no.of element to store:\n");
+    scanf("%d",&size);
+    int a[size];
+    for(int i=0;i<=size;i++)
+    {
+        printf("Enter the %dvalue\n",i+1);
+        scanf("%d",&a[i]);
+    }
+    printf("------------\n{");
+    for(int i=0;i<=size-1;i++)
+    {
+        printf("%d,",a[i]);
+    }
+    printf("}");
+}
